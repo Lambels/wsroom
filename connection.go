a83bd7c52be56e710bdaf7106bc52326fb3c4eec
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-28 18:21:31 +0300

    debug

--- connection.go
+++ connection.go
@@ -1,6 +1,7 @@
 package wsroom
 
 import (
+	"log"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -70,6 +71,7 @@ func (conn Connection) readPump() {
 		msg := conn.room.UnmarshalIn
 
 		err := conn.Conn.ReadJSON(&msg)
+		log.Println(err)
 		if err != nil {
 			break
 		}
